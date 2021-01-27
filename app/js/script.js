20c973da939db05ff5c2a1191d8220e8c6068820
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-01-27 16:12:25 -0400

    Animated Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+    
+    if(header.classList.contains('open')) { // Close Hambuger Menu
+        header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+        
+    } else { // Open Hamburger Menu
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+
+    }
+});
