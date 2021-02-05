cba7560b0d88377a6ff34fdf9d549d88fca87f47
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-02-04 20:49:49 -0400

    Animated Mobile Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,26 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function(){
     
     if(header.classList.contains('open')) { // Close Hambuger Menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
+        fadeElems.forEach(fadeElement => {
+            fadeElement.classList.add('fade-out');
+            fadeElement.classList.remove('fade-in');
+        });
         
     } else { // Open Hamburger Menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeElems.forEach(fadeElem => {
+            fadeElem.classList.add('fade-in');
+            fadeElem.classList.remove('fade-out');
+        });
 
     }
 });
